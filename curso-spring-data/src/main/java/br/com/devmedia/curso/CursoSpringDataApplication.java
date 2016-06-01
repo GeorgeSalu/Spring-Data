2166120e492c3c0c67c25b0e7581c2a7f7323ea2
GeorgeSalu <george.salu10@gmail.com>
2016-06-01 15:51:48 -0300

    start do projeto, projeto spring apara explorar os recursos do spring data

---
+++ curso-spring-data/src/main/java/br/com/devmedia/curso/CursoSpringDataApplication.java
@@ -0,0 +1,17 @@
+package br.com.devmedia.curso;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+
+
+@SpringBootApplication
+public class CursoSpringDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringDataApplication.class, args);
+	}
+
+}
