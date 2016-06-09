17f070d5b051b739f2505e995d974100c37cc44a
GeorgeSalu <george.salu10@gmail.com>
2016-06-09 20:58:10 -0300

    Trabalhando nas classes de Autor

---
+++ blog/src/main/java/br/com/devmedia/blog/service/AutorService.java
@@ -0,0 +1,38 @@
+package br.com.devmedia.blog.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.devmedia.blog.entity.Autor;
+import br.com.devmedia.blog.repository.AutorRepository;
+
+@Service
+@Transactional(readOnly=true,propagation=Propagation.REQUIRED)
+public class AutorService {
+
+	@Autowired
+	private AutorRepository repository;
+	
+	public void save(Autor autor){
+		
+		repository.save(autor);
+	
+	}
+	
+	public List<Autor> findAll(){
+		return repository.findAll();
+	}
+	
+	public Autor findByNome(String nome){
+		return repository.findByNome(nome);
+	}
+	
+	public Autor findById(Long id){
+		return repository.findOne(id);
+	}
+	
+}
